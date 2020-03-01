5ae845caaf17344598e5d62396ab922f6a66ee05
Paweł Ewertowski <s16460@pjwstk.edu.pl>
2020-03-01 16:24:06 +0100

    pierwszy komit

---
+++ Cw1/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cw1
+{
+    class Person {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
---
+++ Cw1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1 {
+    class Program {
+
+
+        public static async Task Main(string[] args)
+        {
+
+
+            var g = 1;
+            var h = "gggg";
+            var t = true;
+            string path = @"C:\Users\s16460\Desktop\projektApd\Cw1";
+            Console.WriteLine($"{g} {h}");
+            Console.WriteLine(g + h + t);
+            Console.WriteLine(path);
+
+            Person newPerson = new Person { FirstName = "mietek", LastName = "brodek" };
+
+            //string url = args.Length > 0 ?args[0] : "https://www.pja.edu.pl";
+            string url = args[0];
+            Console.WriteLine("Hello World!");
+
+            if (url == null)
+            {
+                throw new ArgumentNullException("brak argow");
+            }
+
+
+
+            using (HttpClient http = new HttpClient())  //using zwalnia zasoby
+            {
+                using (var response = await http.GetAsync(url))
+                {
+                    if (response.IsSuccessStatusCode) //cheking if http resp is 200 or so
+                    {
+                        var htmlContent = await response.Content.ReadAsStringAsync();
+                        var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                        var matches = regex.Matches(htmlContent);
+
+                        foreach (var item in matches)
+                        {
+                            Console.WriteLine(item.ToString());
+                        }
+
+                    }
+                }
+
+
+            }
+
+        }
+         
+    }
+}
